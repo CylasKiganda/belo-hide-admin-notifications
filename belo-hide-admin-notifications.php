441a3f0c8db5fd301e3b24ed280518cc1b5c9f14
Cylas Kiganda <kigandacylas@gmail.com>
2023-03-08 20:07:47 +0300

    users html selector added

--- belo-hide-admin-notifications.php
+++ belo-hide-admin-notifications.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'BELO_HIDE_ADMIN_NOTIFICATIONS_VERSION', '1.0.1' );
+define( 'BELO_HIDE_ADMIN_NOTIFICATIONS_VERSION', '1.0.11111' );
 
 /**
  * The code that runs during plugin activation.

92bfd5c6c5d7716333d63182274584989d691777
Cylas Kiganda <kigandacylas@gmail.com>
2023-03-08 22:01:36 +0300

    header

--- admin/class-belo-hide-admin-notifications-admin.php
+++ admin/class-belo-hide-admin-notifications-admin.php
@@ -134,13 +134,7 @@ class Belo_Hide_Admin_Notifications_Admin {
       ">
       <div class="bhan-wrapper" >
          <div class="bhan-header mdb-header bg-brand-light flex-container">
-            <img width="52" height="52" viewBox="0 0 52 52" class="medallion" style="
-               -webkit-filter: drop-shadow(0 1px 4px rgba(0,0,0,.2));
-               filter: drop-shadow(0 1px 4px rgba(0,0,0,.2));
-               margin-right: 1rem;
-               width: 52px;
-               height: 52px;
-               " src="/"/>
+            <img width="52" height="52" viewBox="0 0 52 52" class="medallion" style="-webkit-filter: drop-shadow(0 1px 4px rgba(0,0,0,.2)); filter: drop-shadow(0 1px 4px rgba(0,0,0,.2)); margin-right: 1rem; width: 70px; height: 70px; border-radius: 50px;" src="<?php echo plugin_dir_url( __FILE__ ) .'/logo.png'; ?>"/>
             <h1 style="
                display: inline-block;
                font-size: 1.3125rem;
